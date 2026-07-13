68936811d9ed4db7d085cc91466db760dc0c8439
Shimul Sharma <Shimulsharma12345@gmail.com>
2026-07-14 01:58:37 +0530

    Wrote the liquidate function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -58,6 +58,7 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__TransferFailed();
     error DSCEngine__BreaksHealthFactor(uint256 healthFactor);
     error DSCEngine__MintFailed();
+    error DSCEngine__HealthFactorOk();
 
     ///////////////////////////////STATE VARIABLES////////////////////
 
@@ -65,6 +66,7 @@ contract DSCEngine is ReentrancyGuard {
     uint256 private constant LIQUIDATION_PRECISION = 100; // 50/100 = 50% liquidation threshold.
     uint256 private constant PRECISION = 1e18; // 18 decimals for precision.
     uint256 private constant MIN_HEALTH_FACTOR = 1;
+    uint256 private constant LIQUIDATION_BONUS = 10;
 
     mapping(address token => address priceFeed) private s_priceFeeds; // Token address to price feed address.
     mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited; // User address to token address to amount of collateral deposited.
@@ -206,7 +208,46 @@ contract DSCEngine is ReentrancyGuard {
         i_dsc.burn(amountDscToBurn);
     }
 
-    function liquidate() external {}
+    /*
+     * @param collateral: The ERC20 token address of the collateral you're using to make the protocol solvent again.
+     * This is collateral that you're going to take from the user who is insolvent.
+     * In return, you have to burn your DSC to pay off their debt, but you don't pay off your own.
+     * @param user: The user who is insolvent. They have to have a _healthFactor below MIN_HEALTH_FACTOR
+     * @param debtToCover: The amount of DSC you want to burn to cover the user's debt.
+     *
+     * @notice: You can partially liquidate a user.
+     * @notice: You will get a 10% LIQUIDATION_BONUS for taking the users funds.
+    * @notice: This function working assumes that the protocol will be roughly 150% overcollateralized in order for this
+    to work.
+    * @notice: A known bug would be if the protocol was only 100% collateralized, we wouldn't be able to liquidate
+    anyone.
+     * For example, if the price of the collateral plummeted before anyone could be liquidated.
+     */
+
+    function liquidate(address collateral, address user, uint256 debtToCover)
+        external
+        moreThanZero(debtToCover)
+        nonReentrant
+    {
+        // 1.Check Health factor of the user.(Do they have enough collateral?)
+        uint256 UserHealthFactor = _healthFactor(user);
+        if (UserHealthFactor >= MIN_HEALTH_FACTOR) {
+            revert DSCEngine__HealthFactorOk();
+        }
+
+        /*
+         2.We burn their DSC tokens (debt) and take their collateral.
+         * User that is being liquidated : $140 ETH, $100 DSC
+         * debttoCover = $100. And 10% LIQUIDATION_BONUS
+         * $100 DSC = ?? ETH. ---> 0.05
+         * That means we're giving $110 of WETH for 100 DSC.
+        */
+        uint256 tokenAmountFromDebtCovered = getTokenAmountFromUsd(collateral, debtToCover);
+
+        uint256 bonusCollateral = (tokenAmountFromDebtCovered * LIQUIDATION_BONUS) / PRECISION;
+
+        uint256 totalCollateralToRedeem = (tokenAmountFromDebtCovered + bonusCollateral);
+    }
 
     function getHealthfactor() external view {}
 
@@ -259,6 +300,14 @@ contract DSCEngine is ReentrancyGuard {
             totalCollateralValueInUSD += getUSDValue(token, amount);
         }
         return totalCollateralValueInUSD;
+    }
+
+    function getTokenAmountFromUsd(address token, uint256 UsdamountinWei) public view returns (uint256) {
+        // we're finding how much worth tokens are in Wei.
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // Chainlink price feeds have 8 decimals, so we need to adjust for that.
+        return (UsdamountinWei * 1e8) / uint256(price);
     }
 
     function getUSDValue(address token, uint256 amount) public view returns (uint256) {
